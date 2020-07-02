98384ee47e0e6481241db68dc3f807d3b9945215
Valdemar <arnik.20100608@gmail.com>
2020-07-02 16:33:55 +0300

    Fix

--- Lessons15Map/src/Main.java
+++ Lessons15Map/src/Main.java
@@ -1,6 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
         MyMap mymap = new MyMap();
-        mymap.put("2", "one");
+        Map map = new HashMap();
+        mymap.put("32", "one");
+        System.out.println(mymap);
     }
 }
