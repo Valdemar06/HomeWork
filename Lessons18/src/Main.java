9c4d700854b468d51b88fafc4d57d09402c13ac2
Valdemar <arnik.20100608@gmail.com>
2020-07-13 19:53:00 +0300

    Fix memory

--- Lessons18/src/Main.java
+++ Lessons18/src/Main.java
@@ -6,49 +6,42 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
+        /**
+         * Create file
+         */
+        File file = new File("Test.json");
+        File fileOut = new File("OutStream.out");
+
         long time =  System.currentTimeMillis();
         ObjectMapper mapper = new ObjectMapper();
         ObjectInputStream objectInputStream = new ObjectInputStream
-                (new FileInputStream("OutStream.out"));
+                (new FileInputStream(fileOut));
 
         ObjectOutputStream objectOutputStream = new ObjectOutputStream
-                (new FileOutputStream("OutStream.out"));
+                (new FileOutputStream(fileOut));
         List<Object> list = new ArrayList<>();
-        for (int i = 0; i < 100001 ; i++) {
+        for (int i = 0; i < 1000001 ; i++) {
             list.add(i, new Student(i, i));
         }
         try {
 
             /**
-             * Create file
-             */
-            File file = new File("Test.json");
-            /**
              * Write to file format Json
              */
-            long usedBytesJson = Runtime.getRuntime().totalMemory()
-                    - Runtime.getRuntime().freeMemory();
-            String memory = "Memory (" + usedBytesJson / 1048576 + ")  mb" + " JSON";
-            list.add(memory);
+            long usedBytesJson = file.length()/(1024*1024);
+            String memory = "Memory (" + usedBytesJson +")  mb" + " JSON";
             System.out.println("How much memory is the file: " + memory);
             double first = (double) (System.currentTimeMillis() - time);
             String timeRecordJson = "Record execution JSON: " + first + " ms";
-            list.add(timeRecordJson);
             mapper.writeValue(file, list);
             System.out.println(timeRecordJson);
 
             /**
              * Serialized
              */
-            long usedBytes = Runtime.getRuntime().totalMemory()
-                    - Runtime.getRuntime().freeMemory();
-            String memorySerialized = "Memory (" + usedBytes / 1048576 + ")  mb" + " Serialized";
-            list.remove(memory);
-            list.remove(timeRecordJson);
-            list.add(memorySerialized);
             double middle = (double) (System.currentTimeMillis() - time);
             String timeSerialized = "Record execution Serialized: " + middle + " ms";
-            list.add(timeSerialized);
+            objectOutputStream.writeObject(list);
             System.out.println(timeSerialized);
 
 
@@ -57,10 +50,10 @@ public class Main {
              */
             double deserialize = (double) (System.currentTimeMillis() - time);
             String timeDeserialize = "Read execution Deserialize: " + deserialize + " ms";
-            list.add(timeDeserialize);
-            objectOutputStream.writeObject(list);
             List student = (List) objectInputStream.readObject();
-            System.out.println(student);
+           // System.out.println(student);
+            long usedBytes =fileOut.length()/(1024*1024);
+            String memorySerialized = "Memory (" + usedBytes + ")  mb" + " Serialized";
             System.out.println(timeDeserialize);
             System.out.println("How much memory is the file: " + memorySerialized);
         }catch (IOException io){
