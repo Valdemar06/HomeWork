d4b63bc856867031825eb46f552d6c28df5223c1
Valdemar <arnik.20100608@gmail.com>
2020-07-20 14:22:59 +0300

    Use HashMap

--- Lessons19/src/Main.java
+++ Lessons19/src/Main.java
@@ -5,14 +5,14 @@ public class Main {
     public static void main(String[] args) {
         int sum = 13;
         List<Integer> list = new ArrayList<>(Arrays.asList(3, 7, 10, 15));
-        List<Integer> delete = new ArrayList<>();
-        for (int i : list) {
-            if (list.indexOf(sum - i) == -1){
-                delete.add(Arrays.binarySearch(list.toArray(), sum - i));
-            }else{
-                System.out.println( "["+ Arrays.binarySearch(list.toArray(),(sum - i)) + ", "
-                        + Arrays.binarySearch(list.toArray(), i )+ "]");
+        Map<Integer, Integer> pairs = new HashMap<>();
+        for (int i = 0; i < list.size(); i++) {
+            int complement = sum - list.get(i);
+            if (pairs.containsKey(complement)) {
+                System.out.println(pairs.get(complement) + " " + i);
             }
+            pairs.put(list.get(i), i);
         }
     }
 }
+
