40a4a58f2d8cad58f4322a6cdb71b523c4b98842
Valdemar <arnik.20100608@gmail.com>
2020-07-26 10:54:42 +0300

    Fix

--- BruteForce/src/Main.java
+++ BruteForce/src/Main.java
@@ -1,3 +1,6 @@
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
 public class Main {
     public static void main(String[] args) {
        /* List<String> array = new ArrayList<>();
@@ -13,9 +16,10 @@ public class Main {
         BruteForce bruteForce2 = new BruteForce("5ebe2294ecd0e0f08eab7690d2a6ee69");
         BruteForce bruteForce3 = new BruteForce("13d70e09909669272b19647c2a55dacb");
         BruteForce bruteForce4 = new BruteForce("5f50dfa5385e66ce46ad8d08a9c9be68");
-        bruteForce.run();
-        bruteForce2.run();
-        bruteForce3.run();
-        bruteForce4.run();
+        ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(4);
+        threadPoolExecutor.submit(bruteForce);
+        threadPoolExecutor.submit(bruteForce2);
+        threadPoolExecutor.submit(bruteForce3);
+        threadPoolExecutor.submit(bruteForce4);
     }
 }
