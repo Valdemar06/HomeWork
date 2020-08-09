bdac284a13b3ee581026e78490c8f989b70e4cf7
Valdemar <arnik.20100608@gmail.com>
2020-08-09 12:48:22 +0300

    Fix

--- BruteForce/src/Main.java
+++ BruteForce/src/Main.java
@@ -1,17 +1,9 @@
+import java.io.IOException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 
 public class Main {
-    public static void main(String[] args) {
-       /* List<String> array = new ArrayList<>();
-        FileReader fileReader = new FileReader("C:\\Users\\Valdemar\\IdeaProjects\\Lessons19\\Password.txt");
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
-        String line = bufferedReader.readLine();
-        while (line!= null) {
-            array.add(line);
-            System.out.println(line);
-            line = bufferedReader.readLine();
-        }*/
+    public static void main(String[] args) throws IOException {
         BruteForce bruteForce = new BruteForce("f016441d00c16c9b912d05e9d81d894d");
         BruteForce bruteForce2 = new BruteForce("5ebe2294ecd0e0f08eab7690d2a6ee69");
         BruteForce bruteForce3 = new BruteForce("13d70e09909669272b19647c2a55dacb");
@@ -21,5 +13,6 @@ public class Main {
         threadPoolExecutor.submit(bruteForce2);
         threadPoolExecutor.submit(bruteForce3);
         threadPoolExecutor.submit(bruteForce4);
+        threadPoolExecutor.shutdown();
     }
 }
