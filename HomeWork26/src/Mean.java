6b31482f93b9f67edab572afbed4d6fa96666a79
Valdemar <arnik.20100608@gmail.com>
2020-08-11 14:20:59 +0300

    Use stream

--- HomeWork26/src/Mean.java
+++ HomeWork26/src/Mean.java
@@ -1,30 +1,35 @@
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
+
 
 public class Mean {
 
-    public double meanMethod(List<Double> numbers){
-        /**
-         * mean = n(n+1)/2
-         */
-        double result = numbers.size() * (numbers.size() + 1) >> 1;
+
+    public double meanMethod(List<Double> numbers) {
+     /*   double result = 0;
+        for (double number: numbers) {
+            result += number;
+        }
+        return result / numbers.size();*/
+        Stream<Double> stream = numbers.stream();
+        double  result = stream.mapToDouble(x -> x).sum();
         return result/numbers.size();
     }
     public double quadraticMethod(List<Double> numbers){
-        /**
-         * (n(n+1)*(2n+1))/6(Why division by 6. I don't know)
-         */
-        List<Double>list = new ArrayList<>();
-        double firstResult = (numbers.size()*(numbers.size() +1 )*(2*numbers.size()+1)) /6;
-
+      /* double result = 0;
+       double power =0;
         for (double number: numbers) {
-            if (number%2 == 0){
-                list.add(number);
+            if(number > 0){
+                power = number * number;
+            }
+            if (power%2==0){
+                result +=power;
             }
         }
-        double secondResult = (list.size() *(list.size()+1) * (2* list.size()+1))/6;
-
-        return secondResult;
+        return result;*/
+        Stream<Double> stream = numbers.stream();
+        return stream.filter(doubles -> doubles>0)
+                .map(doubles -> doubles * doubles)
+                .filter(doubles -> doubles%2 ==0).mapToDouble(x->x).sum();
     }
-
 }
