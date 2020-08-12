c91390798eba404351a443f9cb3fc1745d749eb7
Valdemar <arnik.20100608@gmail.com>
2020-08-13 00:52:26 +0300

    Use matches

--- HomeWork27/src/Main.java
+++ HomeWork27/src/Main.java
@@ -2,25 +2,16 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+
 public class Main {
     public static void main(String[] args) {
         /**
-         * A,O,E,I,U
+         * I FIND METHOD MATCHES THIS IS MAGIC MAN
+         * MY TIME WHEN I FINISHED 00:51
          */
         List<String> surname = Arrays.asList("Ivanov", "Grechkin", "Pushkin", "Petrov", "Frackov","aoesdad","aui","oia");
-
-        List<String> collect = surname.stream().map(String::toUpperCase)
-                .filter(name -> name.contains("A") && name.contains("O") && name.contains("E")||
-                        name.contains("A") && name.contains("O") && name.contains("I")||
-                        name.contains("A") && name.contains("O") && name.contains("U")||
-                        name.contains("A") && name.contains("E") && name.contains("I")||
-                        name.contains("A") && name.contains("E") && name.contains("U")||
-                        name.contains("A") && name.contains("I") && name.contains("U")||
-                        name.contains("O") && name.contains("E") && name.contains("I")||
-                        name.contains("O") && name.contains("E") && name.contains("U")||
-                        name.contains("O") && name.contains("I") && name.contains("U")||
-                        name.contains("E") && name.contains("I") && name.contains("U")).
-                collect(Collectors.toList());
+        List<String> collect = surname.stream().
+                filter(s -> s.toUpperCase().matches("(.*[AEYIUO].*){3,}")).collect(Collectors.toList());
         System.out.println(collect);
     }
 }
