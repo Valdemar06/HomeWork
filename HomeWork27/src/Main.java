e6684722dba0570e738c57131a958b3c12d05169
Valdemar <arnik.20100608@gmail.com>
2020-08-14 11:12:13 +0300

    Create class Student

--- HomeWork27/src/Main.java
+++ HomeWork27/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,10 +10,20 @@ public class Main {
          * I FIND METHOD MATCHES THIS IS MAGIC MAN
          * MY TIME WHEN I FINISHED 00:51
          */
-        List<String> surname = Arrays.asList("Ivanov", "Grechkin", "Pushkin", "Petrov", "Frackov","aoesdad","aui","oia");
+        Student student1 = new Student("Ivanov", "Ivan");
+        Student student2 = new Student("Petrov", "Petr");
+        Student student3 = new Student("Pushkin", "Aleksandr");
+        Student student4 = new Student("Marian", "Nikolay");
+        Student student5 = new Student("Kuplinov", "Dmitry");
+        List<Student> surname = new ArrayList<>();
+        surname.add(student1);
+        surname.add(student2);
+        surname.add(student3);
+        surname.add(student4);
+        surname.add(student5);
         List<String> collect = surname.stream().
-                filter(s -> s.toUpperCase().matches("(.*[AEYIUO].*){3,}"))
-                .map(name -> name.toUpperCase()).sorted().collect(Collectors.toList());
+                filter(s -> s.getFirstName().toUpperCase().matches("(.*[AEYIUO].*){3}"))
+                .map(name -> name.getLastName().toUpperCase()).sorted().collect(Collectors.toList());
         System.out.println(collect);
     }
 }
