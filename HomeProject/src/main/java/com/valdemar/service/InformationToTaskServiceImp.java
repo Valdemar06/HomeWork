e36eaca1276d8d8e46dab75b89c59d8fa93e2f91
Valdemar <arnik.20100608@gmail.com>
2020-08-18 18:44:10 +0300

    Use XML file

---
+++ HomeProject/src/main/java/com/valdemar/service/InformationToTaskServiceImp.java
@@ -0,0 +1,41 @@
+package com.valdemar.service;
+
+import com.valdemar.model.InfoToTask;
+
+import com.valdemar.repository.InformationRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class InformationToTaskServiceImp implements InformationToTaskService {
+    private IdGenerator idGenerator;
+    private InformationRepository informationRepository;
+
+
+    public InformationToTaskServiceImp(IdGenerator idGenerator, InformationRepository informationRepository) {
+        this.idGenerator = idGenerator;
+        this.informationRepository = informationRepository;
+
+    }
+
+    @Override
+    public InfoToTask creatInfo(String textToTask ,LocalDateTime time) {
+        String id = idGenerator.generation();
+
+        InfoToTask newInfo = new InfoToTask(id,time,textToTask);
+        informationRepository.save(newInfo);
+        return newInfo;
+    }
+
+    @Override
+    public InfoToTask findInfo(String textToTask) {
+        List<InfoToTask> infos = informationRepository.findAll();
+        return infos.stream().filter(info -> info.getTextToTask().equals(textToTask)).findFirst().get();
+    }
+
+
+    @Override
+    public List<InfoToTask> findAll() {
+        return informationRepository.findAll();
+    }
+}
